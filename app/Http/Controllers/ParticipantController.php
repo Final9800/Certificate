787b36d522ce2cfdc3dd70d195506a281dcd2076
Final9800 <cunardyvincent12@gmail.com>
2020-01-29 11:57:14 +0700

    Participant data

---
+++ app/Http/Controllers/ParticipantController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Participant;
+use Illuminate\Http\Request;
+
+class ParticipantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $participants = Participant::all();
+        return view('addParticipant',compact('participants'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Participant::create([
+            'nama'=>$request->nama,
+            ''
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Participant  $participant
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Participant $participant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Participant  $participant
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Participant $participant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Participant  $participant
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Participant $participant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Participant  $participant
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Participant $participant)
+    {
+        //
+    }
+}
---
+++ resources/views/addParticipant.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <input type="text" name="" placeholder="Input Your Name"><br>
+        <input type="text" name="" placeholder="Input Your Address"><br>
+        <input type="date" name="dob" placeholder="Input Your Birthdate"><br>
+        <input type="text" name="phone" placeholder="Input Your Phone Number"><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
